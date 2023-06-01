c1542f6a5a205879df198b19b614bad81f6e8217
Shubham Vishwakarma <shubham.vish.2633@gmail.com>
2023-06-01 14:48:21 +0530

    navbar: add logo links, scroll down effect

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,9 @@ export default function Home() {
   return (
     <>
       <NavBar/>
-      <div>Hello there.</div>
+      <div className='h-[50vh] bg-yellow-200'>Hello there</div>
+      <div className='h-[50vh] bg-yellow-200'>Hello there</div>
+      <div className='h-[50vh] bg-yellow-200'>Hello there</div>
     </>
     
   )
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,8 +1,49 @@
-import React from 'react'
+"use client";
+
+import clsx from 'clsx';
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react'
 
 const NavBar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset;
+      if (scrollTop > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+
+  }, []);
+
   return (
-    <div className='bg-blue-400 w-full p-4'>NavBar</div>
+    <nav 
+      className={clsx(
+        'bg-white flex justify-center pt-1 items-center w-full transition-all fixed', 
+        `${isScrolled && "h-[4.5rem]"}`,
+        `${!isScrolled && "h-[6rem]"}`)}>
+            <div className='w-90% px-8 flex justify-between items-center'>
+              <div className="">
+                <Image src={!isScrolled ? "/ufo1.png" : "/ufo2.png"} width={64} height={64} alt="logo" />
+              </div>
+              <ul className="flex justify-between items-center gap-10">
+                <li><Link href="/">Emotions</Link></li>
+                <li><Link href="/">Manifesto</Link></li>
+                <li><Link href="/">Self-awareness test</Link></li>
+                <li><Link href="/">Work with us</Link></li>
+              </ul>
+              <button className='bg-black text-white px-6 py-3 rounded-full'>Download app</button>
+            </div>
+    </nav>
   )
 }
 
